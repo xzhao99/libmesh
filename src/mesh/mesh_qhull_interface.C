757f99b42d08e485a92c36a88fccb14dbe576665
John Peterson <jwpeterson@gmail.com>
2014-05-20 14:59:12 -0600

    Attempt to call runQhull with existing Mesh points.
    For some reason, this returns "non-simplicial" facets,
    need to investigate more.

--- src/mesh/mesh_qhull_interface.C
+++ src/mesh/mesh_qhull_interface.C
@@ -27,6 +27,7 @@
 #include "libmesh/unstructured_mesh.h"
 #include "libmesh/point.h"
 #include "libmesh/face_tri3.h"
+#include "libmesh/cell_tet4.h"
 
 // Qhull headers
 
@@ -63,17 +64,91 @@ void QhullInterface::triangulate()
 {
   // Taken directly from Qhull's user_eg3 program
   RboxPoints rbox;
-  rbox.appendPoints("100");
+  rbox.appendPoints("10");
+
+  // Debugging: Try to decipher what these commands should be in the general case.
+  libMesh::out << "rbox.comment()=" << rbox.comment() << std::endl;
+  libMesh::out << "rbox.dimension()=" << rbox.dimension() << std::endl;
+  libMesh::out << "rbox.count()=" << rbox.count() << std::endl;
+  {
+    const Real* coords = rbox.coordinates();
+    unsigned ctr = 0;
+    for (int i=0; i<rbox.dimension() * rbox.count(); i+=rbox.dimension())
+      libMesh::out << "Input vertex " << ctr++ << ": " << coords[i] << " " << coords[i+1] << " " << coords[i+2] << std::endl;
+  }
+
   Qhull qhull;
-  qhull.runQhull(rbox, "");
+  try
+    {
+      qhull.runQhull(rbox, "");
+    }
+  catch (QhullError& e)
+    {
+      libmesh_error_msg("Qhull threw an exception: " << e.what());
+    }
 
-  // Print out the vertices
+
+
+//  // Use the other runQhull interface.
+//
+//  // Take the points currently in the Mesh and use them for defining
+//  // the convex hull...  TODO: handle the LIBMESH_DIM=2 case
+//  // elegantly.
+//
+//  std::vector<Real> coords (_mesh.n_nodes() * 3);
+//  {
+//    MeshBase::node_iterator
+//      it = _mesh.nodes_begin(),
+//      end = _mesh.nodes_end();
+//
+//    unsigned ctr = 0;
+//    for (; it != end; ++it)
+//      {
+//        Node* node = *it;
+//        coords[ctr++] = (*node)(0);
+//        coords[ctr++] = (*node)(1);
+//        coords[ctr++] = (*node)(2);
+//      }
+//  }
+//
+//  // Try to mimic the input string from the example program, although this
+//  // didn't seem to make much difference in the output.
+//  std::ostringstream oss;
+//  oss << "rbox " << "\"" << _mesh.n_nodes() << "\"";
+//  // libMesh::out << "rboxCommand2 = " << oss.str() << std::endl;
+//
+//  // Qhull throws exceptions if there is an error, if they go uncaught
+//  // the program dies with code 1, and without any other messages.  So
+//  // let's be nice and catch them!
+//  Qhull qhull;
+//  try
+//    {
+//      qhull.runQhull(/*rboxCommand2=*/oss.str().c_str(),
+//                     /*pointDimension=*/3,
+//                     /*pointCount=*/_mesh.n_nodes(),
+//                     /*rboxPoints=*/&coords[0],
+//                     /*qhullCommand2=*/"");
+//    }
+//  catch (QhullError& e)
+//    {
+//      libmesh_error_msg("Qhull threw an exception: " << e.what());
+//    }
+
+  // Print some info about the convex hull that was generated
+  libMesh::out << "Number of vertices in convex hull: " << qhull.vertexList().size() << std::endl;
+  libMesh::out << "Number of facets in convex hull: " << qhull.facetList().size() << std::endl;
+
+  // Print out the vertices - this print statement doesn't compile for me
   // QhullVertexList vertices = qhull.vertexList();
-  // libMesh::out << vertices;
+  // std::cout << vertices;
 
-  // Print out the elements
+  // Print out the elements - this print statement doesn't compile for me
   // QhullFacetList facets = qhull.facetList();
-  // libMesh::out << facets;
+  // std::cout << facets;
+
+  // Remove existing points/elements from the Mesh, let it be
+  // recreated by the data in the Qhull object.
+  _mesh.clear();
 
   // Iterate over vertices
   {
@@ -83,12 +158,11 @@ void QhullInterface::triangulate()
 
     for (; it!=end; it=it.next())
       {
-        // libMesh::out << "it.id()=" << it.id() << std::endl;
-
         // x, y, z coordinates
         QhullPoint pt = it.point();
-        // libMesh::out << pt << std::endl;
-        // libMesh::out << pt[0] << ", " << pt[1] << ", " << pt[2] << std::endl;
+
+        // Print vertex information
+        libMesh::out << "Vertex " << it.id() << ": " << pt[0] << ", " << pt[1] << ", " << pt[2] << std::endl;
 
         // Add the point to the libmesh mesh with the Qhull id
         _mesh.add_point(Point(pt[0], pt[1], pt[2]), it.id());
@@ -103,34 +177,58 @@ void QhullInterface::triangulate()
 
     for (; it!=end; it=it.next())
       {
-        // Construct a new Tri3 element... need to generalize to 3D?
+        // Status of this facet
+        libMesh::out << "isDefined()=" << it.isDefined() << std::endl;
+        libMesh::out << "isGood()=" << it.isGood() << std::endl;
+        libMesh::out << "isSimplicial()=" << it.isSimplicial() << std::endl;
+        libMesh::out << "isTopOrient()=" << it.isTopOrient() << std::endl;
+        libMesh::out << "isTriCoplanar()=" << it.isTriCoplanar() << std::endl;
+        libMesh::out << "isUpperDelaunay()=" << it.isUpperDelaunay() << std::endl;
 
-        // Elem* elem = Elem::build(TRI3).release();
-        Elem* elem = new Tri3;
+        QhullVertexSet vset = it.vertices();
+        int n_vertices = vset.count();
 
-        // libMesh::out << "Facet " << it.id() << " has vertices: ";
+        // Qhull facets sometimes have > 3 vertices but aren't tets.  What are they?
+        Elem* elem = NULL;
+
+        switch (n_vertices)
+          {
+          case 3:
+            {
+              elem = new Tri3;
+              break;
+            }
+
+          case 4:
+            {
+              elem = new Tet4;
+              break;
+            }
+
+          default:
+            libmesh_error_msg("Facet has invalid number of vertices = " << n_vertices);
+          }
 
         // Set the qhull ID on this Elem
         elem->set_id() = it.id();
 
-        QhullVertexSet vset = it.vertices();
-        // libMesh::out << "vset has " << vset.count() << " entries." << std::endl;
-
         QhullVertexSet::const_iterator
           vset_it = vset.begin(),
           vset_end = vset.end();
+
+        libMesh::out << "Facet " << it.id() << " has " << n_vertices << " vertices: ";
 
         // Iterate over vertices
         unsigned ctr=0;
         for (; vset_it != vset_end; ++vset_it)
           {
             QhullVertex vertex = *vset_it;
-            // libMesh::out << vertex.id() << " ";
+            libMesh::out << vertex.id() << " ";
 
             // Set the Elem's node pointer.  FIXME: be more careful about NULL pointers.
             elem->set_node(ctr++) = _mesh.node_ptr(vertex.id());
           }
-        // libMesh::out << std::endl;
+        libMesh::out << std::endl;
 
         // Finally, add the element to the Mesh
         _mesh.add_elem(elem);
